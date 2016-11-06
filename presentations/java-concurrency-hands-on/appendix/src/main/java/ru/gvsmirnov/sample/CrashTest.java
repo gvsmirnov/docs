68b87508db5cf37bc15876e2b25312e9cedb7138
Gleb Smirnov <me@gvsmirnov.ru>
2016-11-06 16:19:06 +0200

    Added an english version of mysterious hotspot

--- presentations/java-concurrency-hands-on/appendix/src/main/java/ru/gvsmirnov/sample/CrashTest.java
+++ presentations/java-concurrency-hands-on/appendix/src/main/java/ru/gvsmirnov/sample/CrashTest.java
@@ -1,7 +1,28 @@
 package ru.gvsmirnov.sample;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static java.util.concurrent.ForkJoinPool.commonPool;
+
 public class CrashTest {
 
+    private static boolean finished;
+    private static int value;
 
+    static void executedOnCpu0() {
+        value = 10;
+        finished = true;
+    }
+
+    static int executedOnCpu1() {
+        while(!finished);
+        return value;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new Thread(CrashTest::executedOnCpu0).start();
+        System.out.println(executedOnCpu1());
+    }
 
 }
