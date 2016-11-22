7b8d6afe14c257006624cd66aa6e3e5430c30e9b
Gleb Smirnov <me@gvsmirnov.ru>
2016-11-22 23:06:04 +0200

    Added the topconf talks

--- presentations/java-mysterious-hotspot/appendix/src/MemoryChecker.java
+++ presentations/java-mysterious-hotspot/appendix/src/MemoryChecker.java
@@ -1,6 +1,6 @@
 public class MemoryChecker {
-    public static void main(String[] args) {
-        long maxMemory = Runtime.getRuntime().maxMemory();
+    public static void main(final String[] args) {
+        final long maxMemory = Runtime.getRuntime().maxMemory();
 
         System.out.printf(
                 "Max memory: %d MB", maxMemory / 0x100000
